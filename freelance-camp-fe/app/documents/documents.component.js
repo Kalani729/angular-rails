c7458bad51ddf99b497be0251b5cc73d4480ffb5
Mark Albright <kalani729@gmail.com>
2017-09-06 13:14:18 -0600

    Added card classes to documents list

--- freelance-camp-fe/app/documents/documents.component.js
+++ freelance-camp-fe/app/documents/documents.component.js
@@ -16,21 +16,21 @@ var DocumentsComponent = /** @class */ (function () {
                 description: 'asdfasdfasdf asdfasdf',
                 file_url: 'http://www.google.com',
                 updated_at: '09/05/17',
-                image_url: 'http://www.google.com',
+                image_url: 'https://cdn.pixabay.com/photo/2013/09/06/14/43/brown-179454_960_720.jpg',
             },
             {
                 title: "My Second Doc",
                 description: 'asdfasdfasdf asdfasdf',
                 file_url: 'http://www.google.com',
                 updated_at: '09/05/17',
-                image_url: 'http://www.google.com',
+                image_url: 'https://cdn.pixabay.com/photo/2014/05/03/01/03/macbook-336704_960_720.jpg',
             },
             {
                 title: "My Last Doc",
                 description: 'asdfasdfasdf asdfasdf',
                 file_url: 'http://www.google.com',
                 updated_at: '09/05/17',
-                image_url: 'http://www.google.com',
+                image_url: 'https://cdn.pixabay.com/photo/2014/12/27/15/40/office-581131_960_720.jpg',
             }
         ];
     }
